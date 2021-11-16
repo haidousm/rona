d45ea555728ec70e3c3d83627c019bd7c9316720
haidousm <haidous.m@gmail.com>
2021-11-16 21:18:01 +0200

    create connection details entity

--- src/main/java/com/haidousm/LAURona/Main.java
+++ src/main/java/com/haidousm/LAURona/Main.java
@@ -1,5 +1,6 @@
 package com.haidousm.LAURona;
 
+import com.haidousm.LAURona.entity.ConnectionDetails;
 import com.haidousm.LAURona.entity.User;
 import com.haidousm.LAURona.utils.HibernateUtil;
 import org.hibernate.Transaction;
@@ -13,13 +14,15 @@ public class Main {
                 "jdoe@thedoes.com", "jdoe", "hunter32",
                 true, "/src/new/certs/jdoe.pdf", "/src/new/imgs/jdoe.png");
 
+        ConnectionDetails connectionDetails = new ConnectionDetails("192.168.32.1", 3200, testUser);
+
         Transaction tx = HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
         HibernateUtil.getSessionFactory().getCurrentSession().save(testUser);
+        HibernateUtil.getSessionFactory().getCurrentSession().save(connectionDetails);
         tx.commit();
 
         HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
-        List<User> users = HibernateUtil.getSessionFactory().getCurrentSession().createQuery("from User",
-                User.class).list();
-        users.forEach(System.out::println);
+        List<ConnectionDetails> connectionDetailsList = HibernateUtil.getSessionFactory().getCurrentSession().createQuery("from ConnectionDetails", ConnectionDetails.class).list();
+        connectionDetailsList.forEach(System.out::println);
     }
 }
