cfe5dfb6e67ccaa2f12eca8b74049f43a869f48b
haidousm <haidous.m@gmail.com>
2021-11-22 21:39:48 +0200

    impl basic multithreaded echo server

---
+++ src/main/java/com/haidousm/LAURona/server/ClientHandler.java
@@ -0,0 +1,46 @@
+package com.haidousm.LAURona.server;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable {
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+        try {
+            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.equals("exit")) {
+                    break;
+                }
+                bufferedWriter.write("Server:" + line);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bufferedReader.close();
+                bufferedWriter.close();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/com/haidousm/LAURona/server/Server.java
@@ -0,0 +1,47 @@
+package com.haidousm.LAURona.server;
+
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    private ServerSocket serverSocket;
+    private int port;
+
+    public Server(int port) {
+        this.port = port;
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start() {
+        try {
+
+            while (!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                ClientHandler clientHandler = new ClientHandler(socket);
+                Thread clientThread = new Thread(clientHandler);
+                clientThread.start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stop() {
+
+        try {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
