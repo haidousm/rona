a8636bb745f8801ad3b4bcf020e64f9827240f3e
haidousm <haidous.m@gmail.com>
2021-11-30 20:56:28 +0200

    fix stats table bug after healthstatus table refactor

--- rona-server/src/main/java/com/haidousm/rona/server/handlers/StatsHandler.java
+++ rona-server/src/main/java/com/haidousm/rona/server/handlers/StatsHandler.java
@@ -1,18 +1,20 @@
 package com.haidousm.rona.server.handlers;
 
 import com.google.gson.Gson;
+import com.haidousm.rona.common.entity.User;
 import com.haidousm.rona.common.entity.UserAuthToken;
 import com.haidousm.rona.common.enums.Health;
 import com.haidousm.rona.common.enums.Status;
 import com.haidousm.rona.common.requests.AuthorizedRequest;
 import com.haidousm.rona.common.requests.GenericRequest;
-import com.haidousm.rona.common.requests.Request;
 import com.haidousm.rona.common.requests.builders.AuthorizedRequestBuilder;
 import com.haidousm.rona.common.responses.GenericResponse;
 import com.haidousm.rona.common.responses.StatsResponse;
 import com.haidousm.rona.common.responses.builders.StatsResponseBuilder;
 import com.haidousm.rona.server.utils.HibernateUtil;
 import org.hibernate.Transaction;
+
+import java.util.List;
 
 public class StatsHandler {
     public static GenericResponse handleGetStats(GenericRequest request) {
@@ -36,12 +38,29 @@ public class StatsHandler {
                 statsResponse.setStatus(Status.UNAUTHORIZED);
                 return statsResponse;
             }
-            int numberOfSafeAndVaccinatedUsers = HibernateUtil.getSession().createQuery("select count(*) from HealthStatus where status = :status and user.isVaccinated = true", Long.class).setParameter("status", Health.SAFE).getSingleResult().intValue();
-            int numberOfSafeAndUnVaccinatedUsers = HibernateUtil.getSession().createQuery("select count(*) from HealthStatus where status = :status and user.isVaccinated = false", Long.class).setParameter("status", Health.SAFE).getSingleResult().intValue();
-            int numberOfAtRiskAndVaccinatedUsers = HibernateUtil.getSession().createQuery("select count(*) from HealthStatus where status = :status and user.isVaccinated = true", Long.class).setParameter("status", Health.AT_RISK).getSingleResult().intValue();
-            int numberOfAtRiskAndUnVaccinatedUsers = HibernateUtil.getSession().createQuery("select count(*) from HealthStatus where status = :status and user.isVaccinated = false", Long.class).setParameter("status", Health.AT_RISK).getSingleResult().intValue();
-            int numberOfContagiousAndVaccinatedUsers = HibernateUtil.getSession().createQuery("select count(*) from HealthStatus where status = :status and user.isVaccinated = true", Long.class).setParameter("status", Health.CONTAGIOUS).getSingleResult().intValue();
-            int numberOfContagiousAndUnVaccinatedUsers = HibernateUtil.getSession().createQuery("select count(*) from HealthStatus where status = :status and user.isVaccinated = false", Long.class).setParameter("status", Health.CONTAGIOUS).getSingleResult().intValue();
+            int numberOfSafeAndVaccinatedUsers = 0;
+            int numberOfSafeAndUnVaccinatedUsers = 0;
+            int numberOfAtRiskAndVaccinatedUsers = 0;
+            int numberOfAtRiskAndUnVaccinatedUsers = 0;
+            int numberOfContagiousAndVaccinatedUsers = 0;
+            int numberOfContagiousAndUnVaccinatedUsers = 0;
+            List<User> users = HibernateUtil.getSession().createQuery("from User", User.class).getResultList();
+
+            for (User user : users) {
+                if (user.getHealthStatuses().get(0).getStatus() == Health.SAFE && user.getIsVaccinated()) {
+                    numberOfSafeAndVaccinatedUsers++;
+                } else if (user.getHealthStatuses().get(0).getStatus() == Health.SAFE && !user.getIsVaccinated()) {
+                    numberOfSafeAndUnVaccinatedUsers++;
+                } else if (user.getHealthStatuses().get(0).getStatus() == Health.AT_RISK && user.getIsVaccinated()) {
+                    numberOfAtRiskAndVaccinatedUsers++;
+                } else if (user.getHealthStatuses().get(0).getStatus() == Health.AT_RISK && !user.getIsVaccinated()) {
+                    numberOfAtRiskAndUnVaccinatedUsers++;
+                } else if (user.getHealthStatuses().get(0).getStatus() == Health.CONTAGIOUS && user.getIsVaccinated()) {
+                    numberOfContagiousAndVaccinatedUsers++;
+                } else if (user.getHealthStatuses().get(0).getStatus() == Health.CONTAGIOUS && !user.getIsVaccinated()) {
+                    numberOfContagiousAndUnVaccinatedUsers++;
+                }
+            }
 
             statsResponse = StatsResponseBuilder.builder().setNumberOfSafeAndVaccinatedUsers(numberOfSafeAndVaccinatedUsers).setNumberOfSafeAndUnVaccinatedUsers(numberOfSafeAndUnVaccinatedUsers).setNumberOfAtRiskAndVaccinatedUsers(numberOfAtRiskAndVaccinatedUsers).setNumberOfAtRiskAndUnVaccinatedUsers(numberOfAtRiskAndUnVaccinatedUsers).setNumberOfContagiousAndVaccinatedUsers(numberOfContagiousAndVaccinatedUsers).setNumberOfContagiousAndUnVaccinatedUsers(numberOfContagiousAndUnVaccinatedUsers).build();
             tx.commit();
